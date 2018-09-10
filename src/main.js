158d6ef81a3208d619aef247f918d30d92c9ea86
1105128664@qq.com <1105128664@qq.com>
2018-09-10 15:38:22 +0800

    项目第二天

--- src/main.js
+++ src/main.js
@@ -17,18 +17,27 @@ import './lib/mui/css/icons-extra.css'
 
 
 
-
 // 按需导入 Mint-UI 中的组件
-import { Header, Swipe, SwipeItem } from 'mint-ui'
+import { Header, Swipe, SwipeItem,Button} from 'mint-ui'
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Button.name, Button)
 
 import router from './router.js'
 //导入vue-resource
 import VueResource from 'vue-resource'
 //安装vue-resource
 Vue.use(VueResource)
+
+import moment from 'moment'
+//全局配置根路由，必须在resource后面
+Vue.http.options.root = 'http://127.0.0.1:8090';
+
+Vue.filter('dateFormat',function(datastr,pattern="YYYY-MM-DD HH:mm:ss"){
+    return moment(datastr).format(pattern);
+})
+
 var vm = new Vue({
     el:'#app',
     render: c=>c(app),
